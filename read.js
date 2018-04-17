7c0ccc08dd1edae9ec4b7d623796dd7e6f83baf1
kdncode <kdncode@gmail.com>
2018-04-16 20:00:35 -0500

    Recall async and sync

---
+++ read.js
@@ -0,0 +1,18 @@
+//https://nodejs.org/api/fs.html#fs_fs_readfile_file_options_callback
+let fs = require('fs');
+// // Async 
+
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+//     if (err) return console.log('Error: ' + error);
+//     console.log('My data from test.txt: ',data);
+// });
+// console.log('End');
+
+
+//================//
+// // Sync
+
+// let data = fs.readFileSync('./test.txt', 'utf8');
+// console.log(data);
+
+// console.log('End');
