a3fe10a8c4a37d92192a1d824a4b6f0f8adf796b
kdncode <kdncode@gmail.com>
2018-04-16 20:53:52 -0500

    Recall callback

---
+++ callback.js
@@ -0,0 +1,36 @@
+let square = (a, b, h) => (a + b) * h / 2;
+
+console.log('Square is ' + square(2,3, 5));
+
+// let add = (a, b, cb) => {
+//     setTimeout( () => {
+//         let error, results;
+//         if(typeof a != 'number' || typeof b !== 'number') {
+//             error = 'Must be a number';
+//             return cb(error, results)
+//         }
+//         results = a + b;
+//         cb(error, results);
+//     }, 1000);
+// }
+
+// add(5, 3, (err, result) => {
+//     if(err) return console.log('Error: '+ err);
+//     console.log('Result: ' + result);
+// });
+
+
+// Or 
+let add = (a, b, cb) => {
+    setTimeout( () => {
+        if ( typeof a != 'number' || typeof b != 'number') {
+            return cb(new Error('Parameter must be a number'));
+        }
+        cb(undefined, a + b);
+    }, 1000);
+}
+
+add(5, 3, (err, result) => {
+    if(err) return console.log('Error: '+ err);
+    console.log('Result: ' + result);
+});
