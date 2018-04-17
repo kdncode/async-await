c98cfdfe62d3b530fdfa9d55007ca92e46acde0e
kdncode <kdncode@gmail.com>
2018-04-16 19:05:30 -0500

    added var, let and arrow

---
+++ index.js
@@ -0,0 +1,54 @@
+// Var & Let
+
+// var a = 5;
+// console.log(a)
+
+// let b = 10;
+// console.log(b)
+
+// =============== //
+
+// Scope 
+
+// if(true) {
+//     var c = 15;
+// }
+
+// console.log('c = ', c)
+
+
+// if(true) {
+//     let c = 15;
+//     console.log('Inside scope', c)
+// }
+
+// console.log('c = ', c)
+
+// =============== //
+
+// Arrow 
+// let arr = [3, 4, 1, 0, 5];
+
+// arr.forEach(function(e) {
+//     console.log(e);
+    
+// });
+// console.log("=======");
+
+// // Or
+// arr.forEach( (e) => {
+//     console.log(e);
+// });
+
+// console.log("=======");
+
+// Or If there is only ONE parameter - Then you can omit ( )
+// arr.forEach( e => {
+//     console.log(e);
+// });
+
+// console.log("=======");
+
+// Or there is only ONE 
+// arr.forEach( e => console.log(e));
+// console.log("=======");
