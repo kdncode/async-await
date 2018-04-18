f68ba24dfc5b93c465f1ddeee31ff12ef9bdcf83
kdncode <kdncode@gmail.com>
2018-04-17 23:24:12 -0500

    Update read.js

--- read.js
+++ read.js
@@ -1,18 +1,18 @@
 //https://nodejs.org/api/fs.html#fs_fs_readfile_file_options_callback
 let fs = require('fs');
-// // Async 
+// Async 
 
-// fs.readFile('./test.txt', 'utf8', (err, data) => {
-//     if (err) return console.log('Error: ' + error);
-//     console.log('My data from test.txt: ',data);
-// });
-// console.log('End');
+fs.readFile('./test.txt', 'utf8', (err, data) => {
+    if (err) return console.log('Error: ' + error);
+    console.log('My data from test.txt: ',data);
+});
+console.log('End');
 
 
-//================//
-// // Sync
+================//
+// Sync
 
-// let data = fs.readFileSync('./test.txt', 'utf8');
-// console.log(data);
+let data = fs.readFileSync('./test.txt', 'utf8');
+console.log(data);
 
-// console.log('End');
+console.log('End');
