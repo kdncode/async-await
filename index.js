7aece79f15237359aa177cdc7ec0cc47d16be639
kdncode <kdncode@gmail.com>
2018-04-17 23:23:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
-// Var & Let
+//Var & Let
 
-// var a = 5;
-// console.log(a)
+var a = 5;
+console.log(a)
 
-// let b = 10;
-// console.log(b)
+let b = 10;
+console.log(b)
 
-// =============== //
+=============== //
 
-// Scope 
+//Scope 
 
-// if(true) {
-//     var c = 15;
-// }
+if(true) {
+    var c = 15;
+}
 
-// console.log('c = ', c)
+console.log('c = ', c)
 
 
-// if(true) {
-//     let c = 15;
-//     console.log('Inside scope', c)
-// }
+if(true) {
+    let c = 15;
+    console.log('Inside scope', c)
+}
 
-// console.log('c = ', c)
+console.log('c = ', c)
 
-// =============== //
+=============== //
 
-// Arrow 
-// let arr = [3, 4, 1, 0, 5];
+//Arrow 
+let arr = [3, 4, 1, 0, 5];
 
-// arr.forEach(function(e) {
-//     console.log(e);
+arr.forEach(function(e) {
+    console.log(e);
     
-// });
-// console.log("=======");
+});
+console.log("=======");
 
-// // Or
-// arr.forEach( (e) => {
-//     console.log(e);
-// });
+// Or
+arr.forEach( (e) => {
+    console.log(e);
+});
 
-// console.log("=======");
+console.log("=======");
 
-// Or If there is only ONE parameter - Then you can omit ( )
-// arr.forEach( e => {
-//     console.log(e);
-// });
+//Or If there is only ONE parameter - Then you can omit ( )
+arr.forEach( e => {
+    console.log(e);
+});
 
-// console.log("=======");
+console.log("=======");
 
-// Or there is only ONE 
-// arr.forEach( e => console.log(e));
-// console.log("=======");
+//Or there is only ONE 
+arr.forEach( e => console.log(e));
+console.log("=======");
 
-// =============== //
-// Create an array from a given array
-// let array = [4, 5, 7,2, 6];
+=============== //
+//Create an array from a given array
+let array = [4, 5, 7,2, 6];
 
-// // array.forEach(e => console.log(e));
+// array.forEach(e => console.log(e));
 
-// array2 = array.map(function(e) {
-//     return e * 2;
-// })
+array2 = array.map(function(e) {
+    return e * 2;
+})
 
-// // Or this... Because => replaces 'return'
-// array2 = array.map(e =>  e * 2);
+// Or this... Because => replaces 'return'
+array2 = array.map(e =>  e * 2);
 
-// console.log(array2);
+console.log(array2);
 
-// =============== //
-// function add(a, b) {
-//     return a + b;
-// }
+=============== //
+function add(a, b) {
+    return a + b;
+}
 
-// // Or use Arrow function
-// let add = (a, b) => {
-//     return a + b;
-// }
+// Or use Arrow function
+let add = (a, b) => {
+    return a + b;
+}
 
-// console.log(add(3, 2));
+console.log(add(3, 2));
 
-// // Or this
-// let add = (a, b) => a + b;
-// console.log(add(1, 3));
+// Or this
+let add = (a, b) => a + b;
+console.log(add(1, 3));
