10caf9370ca127f3a80bc6d252847da86817689d
kdncode <kdncode@gmail.com>
2018-04-17 23:23:11 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,20 +1,20 @@
-// let getFunc = (num) => {
-//     if ( num >= 0 ) {
-//         return () => { console.log('Positive')}
-//     } else {
-//         return () => { console.log('Negative')}
-//     }
-// }
+let getFunc = (num) => {
+    if ( num >= 0 ) {
+        return () => { console.log('Positive')}
+    } else {
+        return () => { console.log('Negative')}
+    }
+}
 
-// getFunc(3)();
+getFunc(3)();
 
 // Or 
-// let getFunc = (num) => {
-//     if ( num >= 0 ) return () => console.log('Positive');
-//     return () => console.log('Negative');
-// }
+let getFunc = (num) => {
+    if ( num >= 0 ) return () => console.log('Positive');
+    return () => console.log('Negative');
+}
 
-// getFunc(-3)();
+getFunc(-3)();
 
-// Need () after getFunc to invoke the function 
+//Need () after getFunc to invoke the function 
 //otherwise it just returns the func
