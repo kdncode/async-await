1e3ae8c640ac9b189a0f9e1bca7148803807e2bb
kdncode <kdncode@gmail.com>
2018-04-19 17:13:46 -0500

    added return keyword

--- function.js
+++ function.js
@@ -1,3 +1,17 @@
+// Return keyword
+function capitalize(str) {
+    if(typeof str === 'number') {
+        return 'That is not a string'
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+var hi = 'hello word'
+var newHi = capitalize(hi)
+
+var num = 22
+var newNum = capitalize(num);
+
 let getFunc = (num) => {
     if ( num >= 0 ) {
         return () => { console.log('Positive')}
