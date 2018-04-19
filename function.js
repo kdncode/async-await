a64ba7b3dbcbb98b6d116415d31a396277d77864
kdncode <kdncode@gmail.com>
2018-04-19 17:55:00 -0500

    added some practice problems

--- function.js
+++ function.js
@@ -48,3 +48,52 @@ getFunc(-3)();
 
 //Need () after getFunc to invoke the function 
 //otherwise it just returns the func
+
+// Practice Problem
+
+// Write a function isEven() which takes a single numeric argument 
+// and returns true if the number is even, and false otherwise.
+function isEven(myNumber) {
+    if( myNumber % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+
+isEven(4)
+
+// Or use this
+function isEven(myNumber) {
+    return myNumber % 2 === 0;
+}
+
+// Factorial() .... Ex: 3! = 3 * 2 * 1
+// Write a function factorial() which takes a single numeric argument
+// and returns the factorial of that  number
+
+function factorial(num) {
+    
+    // Define a result var
+    var result = 1;
+
+    // Calculate factorial & store in result
+    for( var i = 2; i <= num; i++ ){
+        result *= i
+    }
+    return result;
+}
+
+factorial(3)
+
+// kebabToSnake().... Ex: replace hello-world to hello_world
+// Write a func kebabToSnake() which take a single kebab-cased string
+// arugment and returns the snake_cased ver.
+
+function kebabToSnake(str) {
+
+    // Replace all '-' with '_'
+    var newStr = str.replace(/-/g, '_');
+    return newStr;
+}
+
+kebabToSnake('Hello-World')
