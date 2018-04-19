575c4cc3d0835af4b585eec1c7a8d93fbd0bbc8c
kdncode <kdncode@gmail.com>
2018-04-18 21:58:24 -0500

    added while loop exercises

---
+++ whileLoop.js
@@ -0,0 +1,32 @@
+//While
+console.log('Print all nums btw -10 & 19')
+var i = -10;
+while(i <= 19) {
+    console.log(i);
+    i++;
+}
+
+console.log('Print all even nums btw 10 & 40')
+var i = 10;
+while(i <= 40) {
+    console.log(i);
+    i+=2;
+}
+
+console.log('Print all odd nums btw 300 & 333')
+var i = 300; 
+while(i <= 333) {
+    if( i % 2 !== 0 ){
+        console.log(i)
+    }
+    i++;
+}
+
+console.log('Print all nums divisible by 5 AND 3 btw 5 & 50')
+var i = 5;
+while(i <= 50) {
+    if ( i % 5 == 0 & i % 3 == 0){
+        console.log(i)
+    }
+    i++
+}
