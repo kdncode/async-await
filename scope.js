dffb434eac24f1d6c4854cebe4e9ac411fe6f3e1
kdncode <kdncode@gmail.com>
2018-04-19 18:02:05 -0500

    scope

---
+++ scope.js
@@ -0,0 +1,18 @@
+function add() {
+    var x = 10;
+    console.log(x)
+}
+
+add()
+
+// Another one
+
+var hi = 'hello world'
+
+function doSt() {
+    var hi = 'bye bye'
+    console.log(hi)
+}
+
+doSt()
+console.log(hi)
