b6cf3d18f2cea105414078ce1b7196d2e8122097
kdncode <kdncode@gmail.com>
2018-04-19 20:50:27 -0500

    added for and forEach problems

--- forLoop.js
+++ forLoop.js
@@ -25,3 +25,57 @@ for( i = 5; i <= 50; i++ ) {
         console.log(i)
     }
 }
+
+// Print Reverse array
+function printReverse(arr) {
+    for(var i = arr.length - 1; i >= 0; i-- ) {
+        console.log(arr[i])
+    }
+}
+
+printReverse([2, 5, 1, 7, 3])
+
+
+// CHeck uniForm
+function isUniform(arr) {
+    var first = arr[0];
+
+    for(var i = 1; i < arr.length; i++ ) {
+        if(arr[i] !== first ){
+            return false
+        } 
+        
+    }
+    return true;
+}
+
+console.log(isUniform([3, 3, 4]))
+console.log(isUniform([3, 3, 3]))
+
+// Summary array
+function sumArray(arr) {
+    var total = 0;
+
+    arr.forEach(function(element) {
+        total += element;
+    })
+    return total;
+}
+
+console.log(sumArray([2, 3, 5]))
+
+
+// Max num in array
+function max(arr) {
+    var max = arr[0];
+
+    for(var i = 1; i < arr.length; i++) {
+        if ( arr[i] > max ) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+console.log(max([3, 6, 2]))
